906d3c5566164d0d8dbcd83dd539e6fc2d6e7d7f
aaryaa <maharajaaryaa@gmail.com>
2023-04-08 15:59:30 +0530

    py code revised

--- index.py
+++ index.py
@@ -1,13 +1,9 @@
-import os
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
-
-
-os.environ["PATH"]+=r"C:\DRIVERS\SeleniumDrivers"
 
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
@@ -16,10 +12,16 @@ driver = webdriver.Chrome(options=chrome_options)
 
 driver.get('https://gateway1.iiti.ac.in:8003/index.php?zone=iitiauth')
 driver.implicitly_wait(30)
-# username = driver.find_element_by_id('auth_user')
-username = By.CLASS_NAME("auth_user")
-# password = driver.find_element_by_id('auth_pass')
-password = By.CLASS_NAME("auth_pass")
- 
-username.send_keys("cse210001065@iiti.ac.in")
-password.send_keys("9908119320")
+
+username = driver.find_element(By.ID,"auth_user")
+password = driver.find_element(By.ID,"auth_pass")
+btn = driver.find_element(By.CSS_SELECTOR,"[class='login login-submit']")
+
+# Enter your username and password below
+user = "..."   
+pwrd = "..."
+
+username.send_keys(user)
+password.send_keys(pwrd)
+
+btn.click()
