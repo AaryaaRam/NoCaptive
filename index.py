485a7bcdcc9c19ef3eab54a7b8ca9061fb50dc98
aaryaa <maharajaaryaa@gmail.com>
2023-04-08 17:42:33 +0530

    trigger switch py code added

--- index.py
+++ index.py
@@ -1,27 +1,49 @@
-
+import threading
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
-chrome_options = Options()
-chrome_options.add_experimental_option("detach", True)
+trigger_switch = [True]
 
-driver = webdriver.Chrome(options=chrome_options)
+def switch_listener():
+    while True:
+        new_switch_value=input("Enter (on/off): ")
+        if new_switch_value == "on":
+            trigger_switch[0]=True
+        elif new_switch_value == "off":
+            trigger_switch[0]=False
+            
+switch_thread = threading.Thread(target=switch_listener)
+switch_thread.start()
 
-driver.get('https://gateway1.iiti.ac.in:8003/index.php?zone=iitiauth')
-driver.implicitly_wait(30)
+while True:
+    if trigger_switch[0]:
+        try:
+            chrome_options = Options()
+            chrome_options.add_argument('--headless') 
+            # chrome_options.add_experimental_option("detach", True)
 
-username = driver.find_element(By.ID,"auth_user")
-password = driver.find_element(By.ID,"auth_pass")
-btn = driver.find_element(By.CSS_SELECTOR,"[class='login login-submit']")
+            driver = webdriver.Chrome(options=chrome_options)
+            driver.get('https://gateway1.iiti.ac.in:8003/index.php?zone=iitiauth')
 
-# Enter your username and password below
-user = "..."   
-pwrd = "..."
+            driver.implicitly_wait(5)
+            
+            username = driver.find_element(By.ID,"auth_user")
+            password = driver.find_element(By.ID,"auth_pass")
+            btn = driver.find_element(By.CSS_SELECTOR,"[class='login login-submit']")
 
-username.send_keys(user)
-password.send_keys(pwrd)
+            # Enter your username and password below
+            user = "your_username"   
+            pwrd = "your_password"
 
-btn.click()
+            username.send_keys(user)
+            password.send_keys(pwrd)
+
+            btn.click()
+
+        except:
+            print("You are already connected")
+
+    else:
+        driver.quit()
